d2b9c4c7187db7e03af1dd510ce48e3585c39900
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-02-26 12:47:24 -0500

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -337,7 +337,7 @@ draw = ImageDraw.Draw(image2)
 if get_user_input.b_findings==3:
     image2.paste(image_bl_G1, (145, 958), mask=image_bl_G1)
 if get_user_input.b_findings==4:
-    bl = image_bl_G1.convert("RGB")
+    bl = image_bl_G1.convert("RGBA")
     bl_data = bl.getdata()
     bl_data.putdata([255, 204, 100])
     bl.save("bl4.png")
