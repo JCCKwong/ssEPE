75814b3e2fa9228c842d0d7003ad8a5d409d1d2e
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-03-09 00:35:59 -0500

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -180,6 +180,13 @@ def load_images():
     image_tr_G3 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason3.png'))
     image_tr_G4 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason4.png'))
     image_tr_G5 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason5.png'))
+    ROC_train = PIL.Image.open('Performance Metrics/ROC train.png')
+    ROC_test = PIL.Image.open('Performance Metrics/ROC test.png')
+    PRC_train = PIL.Image.open('Performance Metrics/PRC train.png')
+    PRC_test = PIL.Image.open('Performance Metrics/PRC test.png')
+    Cal_train = PIL.Image.open('Performance Metrics/Calibration train.png')
+    Cal_test = PIL.Image.open('Performance Metrics/Calibration test.png')
+    DCA = PIL.Image.open('Performance Metrics/DCA.png')
     return image2, image_bl_G1, image_bl_G2, image_bl_G3, image_bl_G4, image_bl_G5, \
            image_ml_G1, image_ml_G2, image_ml_G3, image_ml_G4, image_ml_G5, \
            image_al_G1, image_al_G2, image_al_G3, image_al_G4, image_al_G5, \
@@ -187,7 +194,8 @@ def load_images():
            image_br_G1, image_br_G2, image_br_G3, image_br_G4, image_br_G5, \
            image_mr_G1, image_mr_G2, image_mr_G3, image_mr_G4, image_mr_G5, \
            image_ar_G1, image_ar_G2, image_ar_G3, image_ar_G4, image_ar_G5, \
-           image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5
+           image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5, \
+           ROC_train, ROC_test, PRC_train, PRC_test, Cal_train, Cal_test, DCA
 
 
 image2, image_bl_G1, image_bl_G2, image_bl_G3, image_bl_G4, image_bl_G5, \
@@ -197,7 +205,8 @@ image_tl_G1, image_tl_G2, image_tl_G3, image_tl_G4, image_tl_G5, \
 image_br_G1, image_br_G2, image_br_G3, image_br_G4, image_br_G5, \
 image_mr_G1, image_mr_G2, image_mr_G3, image_mr_G4, image_mr_G5, \
 image_ar_G1, image_ar_G2, image_ar_G3, image_ar_G4, image_ar_G5, \
-image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5 = load_images()
+image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5, \
+ROC_train, ROC_test, PRC_train, PRC_test, Cal_train, Cal_test, DCA = load_images()
 
 # Define choices and labels for feature inputs
 CHOICES = {0: 'No', 1: 'Yes'}
@@ -626,6 +635,25 @@ with st.beta_expander("See how the model was developed"):
           **reduction in avoidable non-nerve-sparing radical prostatectomies by 36 vs 32 per 100 patients at\
            a threshold probability of 0.4**.')
     st.write("""""")
+
+    # Performance metric figures
+    my_dpi = 300
+    performance_metrics = plt.figure(dpi=my_dpi)#, figsize=(6, 6)
+    ax1 = fig.add_subplot(2, 3, 1)
+    ax1.imshow(ROC_train)
+    ax2 = fig.add_subplot(2, 3, 2)
+    ax2.imshow(PRC_train)
+    ax3 = fig.add_subplot(2, 3, 3)
+    ax3.imshow(Cal_train)
+    ax4 = fig.add_subplot(2, 3, 4)
+    ax4.imshow(ROC_test)
+    ax5 = fig.add_subplot(2, 3, 5)
+    ax5.imshow(PRC_test)
+    ax6 = fig.add_subplot(2, 3, 6)
+    ax6.imshow(Cal_test)
+    st.image(performance_metrics, use_column_width='auto')
+
+    """
     colA, colB, colC = st.beta_columns([1, 1, 1])
     ROC_train = PIL.Image.open('Performance Metrics/ROC train.png')
     ROC_test = PIL.Image.open('Performance Metrics/ROC test.png')
@@ -640,6 +668,7 @@ with st.beta_expander("See how the model was developed"):
     colB.image(PRC_test, use_column_width='auto')
     colC.image(Cal_train, use_column_width='auto')
     colC.image(Cal_test, use_column_width='auto')
+    """
     st.image(DCA, width=500)
     st.write("""""")
     st.write('This model was developed in accordance to the STREAM-URO framework (see table below).')
