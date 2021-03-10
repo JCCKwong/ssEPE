5797cf66a4266a891e72fc9923ae4f880cbf4365
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-03-09 22:43:21 -0500

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -180,14 +180,8 @@ def load_images():
     image_tr_G3 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason3.png'))
     image_tr_G4 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason4.png'))
     image_tr_G5 = PIL.ImageOps.mirror(PIL.Image.open('Images/TZ_Gleason5.png'))
-    #ROC_train = PIL.Image.open('Performance Metrics/ROC train.png')
-    #ROC_test = PIL.Image.open('Performance Metrics/ROC test.png')
-    #PRC_train = PIL.Image.open('Performance Metrics/PRC train.png')
-    #PRC_test = PIL.Image.open('Performance Metrics/PRC test.png')
-    #Cal_train = PIL.Image.open('Performance Metrics/Calibration train.png')
-    #Cal_test = PIL.Image.open('Performance Metrics/Calibration test.png')
-    #DCA = PIL.Image.open('Performance Metrics/DCA.png')
-    metrics = PIL.Image.open('Performance Metrics/Picture1.png')
+    metrics = PIL.Image.open('Performance Metrics/Performance metrics.png')
+    stream_uro = PIL.Image.open('Performance Metrics/ssEPE STREAM-URO.png')
     return image2, image_bl_G1, image_bl_G2, image_bl_G3, image_bl_G4, image_bl_G5, \
            image_ml_G1, image_ml_G2, image_ml_G3, image_ml_G4, image_ml_G5, \
            image_al_G1, image_al_G2, image_al_G3, image_al_G4, image_al_G5, \
@@ -196,7 +190,7 @@ def load_images():
            image_mr_G1, image_mr_G2, image_mr_G3, image_mr_G4, image_mr_G5, \
            image_ar_G1, image_ar_G2, image_ar_G3, image_ar_G4, image_ar_G5, \
            image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5, \
-           metrics #ROC_train, ROC_test, PRC_test, PRC_train, Cal_train, Cal_test, DCA
+           metrics, stream_uro
 
 
 image2, image_bl_G1, image_bl_G2, image_bl_G3, image_bl_G4, image_bl_G5, \
@@ -207,7 +201,7 @@ image_br_G1, image_br_G2, image_br_G3, image_br_G4, image_br_G5, \
 image_mr_G1, image_mr_G2, image_mr_G3, image_mr_G4, image_mr_G5, \
 image_ar_G1, image_ar_G2, image_ar_G3, image_ar_G4, image_ar_G5, \
 image_tr_G1, image_tr_G2, image_tr_G3, image_tr_G4, image_tr_G5, \
-metrics = load_images()
+metrics, stream_uro = load_images()
 
 # Define choices and labels for feature inputs
 CHOICES = {0: 'No', 1: 'Yes'}
@@ -642,7 +636,6 @@ with st.beta_expander("See how the model was developed"):
     st.write("""""")
     st.write('This model was developed in accordance to the STREAM-URO framework (see table below).')
     st.write("""""")
-    stream_uro = PIL.Image.open('Performance Metrics/ssEPE STREAM-URO.png')
     st.image(stream_uro, width=700)
     st.write("""""")
 st.write("""""")
