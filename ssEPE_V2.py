bf66a4d1ed24d8b9ff2478f9c13a82c1c248ddbb
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-03-12 22:30:36 -0500

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -418,7 +418,7 @@ features_list = ('Age',
                  'Mid % core involvement',
                  'Transition zone % core involvement')
 with col2:
-    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], user_input.iloc[0,:], matplotlib=True, text_rotation=10,
+    st_shap(shap.force_plot(explainer.expected_value, shap_values[0,:], user_input.iloc[0,:], text_rotation=10,
                             plot_cmap=['#e50000','#0343df']))
 #shap.force_plot(explainer.expected_value, shap_values, user_input, features_list, matplotlib=True, text_rotation=10)
 #col2.pyplot(bbox_inches='tight', dpi=600, pad_inches=0, use_column_width='auto')
