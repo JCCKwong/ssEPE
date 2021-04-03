a96a81554b0ba67520b06b9e3507d12366b98584
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-04-03 02:47:33 -0400

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -78,7 +78,7 @@ def load_items():
     model = joblib.load(f_checkpoint)
     features = joblib.load(f_checkpoint1)
     if not f_checkpoint2.exists():
-        explainer = shap.TreeExplainer(model, model_output='probability')
+        explainer = shap.TreeExplainer(model)
         #explainer = shap.TreeExplainer(model, np.array(features), model_output='probability')
         joblib.dump(explainer, f_checkpoint2)
     explainer2 = joblib.load(f_checkpoint2)
