c7a95ed01d67ef0524186074e6704108a8d20dfa
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-07-03 19:53:47 -0400

    Update ssEPE_V2_demo.py

--- ssEPE_V2_demo.py
+++ ssEPE_V2_demo.py
@@ -464,28 +464,28 @@ def full_app(session_state):
                 plt.clf()
 
             ### COMPARISON TO STUDY POPULATION ###
-            left_option = col_left.selectbox("Left lobe: select feature to compare", features_list, index=0)
-            idx = features_list.index(left_option)
-            shap.plots.scatter(model_shap[:, idx], hist=True, dot_size=5, show=False)
-            plt.ylabel('Impact on probability of ssEPE')
+            #left_option = col_left.selectbox("Left lobe: select feature to compare", features_list, index=0)
+            for idx in features_list.index():
+            #idx = features_list.index(left_option)
+                shap.plots.scatter(model_shap[:, idx], hist=True, dot_size=5, show=False)
+                plt.ylabel('Impact on probability of ssEPE')
 
-            # plot patient specific value
-            if submitted:
-                x_pt = np.array(pt_features)[:, idx]
-                y_pt = shap_values[:, idx]
-                plt.plot(x_pt, y_pt, 'ro', markersize=7, alpha=1)
-                col_left2.pyplot(bbox_inches='tight', dpi=600, pad_inches=0, use_column_width='auto')
-
-            if left_option == 'Perineural invasion':
-                positions = (0, 1)
-                x_labels = ('No', 'Yes')
-                plt.xticks(positions, x_labels, rotation=0)
-
-            if left_option == 'Base findings' or left_option == 'Worst Gleason Grade Group':
-                positions = (0, 1, 2, 3, 4, 5, 6, 7)
-                x_labels = ('Normal', 'HGPIN', 'ASAP', 'GGG1', 'GGG2', 'GGG3', 'GGG4', 'GGG5')
-                plt.xticks(positions, x_labels, rotation=0)
-
+                # plot patient specific value
+                if submitted:
+                    x_pt = np.array(pt_features)[:, idx]
+                    y_pt = shap_values[:, idx]
+                    plt.plot(x_pt, y_pt, 'ro', markersize=7, alpha=1)
+    
+                if idx == 'Perineural invasion':
+                    positions = (0, 1)
+                    x_labels = ('No', 'Yes')
+                    plt.xticks(positions, x_labels, rotation=0)
+    
+                if idx == 'Base findings' or idx == 'Worst Gleason Grade Group':
+                    positions = (0, 1, 2, 3, 4, 5, 6, 7)
+                    x_labels = ('Normal', 'HGPIN', 'ASAP', 'GGG1', 'GGG2', 'GGG3', 'GGG4', 'GGG5')
+                    plt.xticks(positions, x_labels, rotation=0)
+    
             col_left2.pyplot(bbox_inches='tight', dpi=600, pad_inches=0, use_column_width='auto')
 
             right_option = col_right.selectbox("Right lobe: select feature to compare", features_list, index=9)
