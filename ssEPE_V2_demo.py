6897e07ff88176790b5de283b8f4f1c4fbae5887
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2021-07-03 20:03:16 -0400

    Update ssEPE_V2_demo.py

--- ssEPE_V2_demo.py
+++ ssEPE_V2_demo.py
@@ -688,19 +688,16 @@ def dev(session_state):
     
     st.header("Additional model explanations")
     st.write("""""")
-    colA, colB = st.beta_columns([1, 1])
+    colA, colB = st.beta_columns([1, 2])
     colA.write("**Feature importance rankings:** helps identify which features had the overall greatest impact on\
                  our ML model's predictions. Here, we see that PSA, Maximum % core involvement, and perineural invasion\
                  were the three most important features in our ML model.")
-    colB.image(summary, use_column_width='auto')
-    st.write("""""")
-    st.write("""""")
-    colD, colE = st.beta_columns([1, 3])
-    colD.write('**Partial dependence plots:** allows us to visualize how a given feature can impact the probability of \
+    colA.image(summary, use_column_width='auto')
+    colB.write('**Partial dependence plots:** allows us to visualize how a given feature can impact the probability of \
                  ssEPE across all its possible values (ie: how does % Gleason pattern 4/5, from 0 to 100%, positively or\
                   negatively impact probability of ssEPE?). We see that our ML model represents each feature in different\
                  ways. Some have a linear or logarithmic relationship, while others are more complex.')
-    colE.image(pdp, use_column_width='auto')
+    colB.image(pdp, use_column_width='auto')
 
 
 if __name__ == "__main__":
