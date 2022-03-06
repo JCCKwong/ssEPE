d9f9047c491c0082b9d253586a456017fe7436e9
JCCKwong <62458997+JCCKwong@users.noreply.github.com>
2022-03-06 12:00:23 -0500

    Update ssEPE_V2.py

--- ssEPE_V2.py
+++ ssEPE_V2.py
@@ -2,14 +2,17 @@
 Title: Explainable artificial intelligence to predict the risk of side-specific extraprostatic extension in
 pre-prostatectomy patients
 
-Authors: Jethro CC. Kwong (1,2), Adree Khondker (3), Christopher Tran (3), Emily Evans (3), Amna Ali (4),
-Munir Jamal (1), Thomas Short (1), Frank Papanikolaou (1), John R. Srigley (5), Andrew H. Feifer (1,4)
+Authors: Jethro CC. Kwong (1,2), Adree Khondker (3), Christopher Tran (3), Emily Evans (3), Adrian I. Cozma (4), Ashkan Javidan (3), Amna Ali (5),
+Munir Jamal (1), Thomas Short (1), Frank Papanikolaou (1), John R. Srigley (6), Benjamin Fine (5,7,8), Andrew H. Feifer (1,5)
 
 (1) Division of Urology, Department of Surgery, University of Toronto, Toronto, ON, Canada
 (2) Temerty Centre for AI Research and Education in Medicine, University of Toronto, Toronto, Canada
 (3) Temerty Faculty of Medicine, University of Toronto, Toronto, ON, Canada
-(4) Institute for Better Health, Trillium Health Partners, Mississauga, ON, Canada
-(5) Department of Laboratory Medicine and Pathobiology, University of Toronto, Toronto, ON, Canada
+(4) Department of Radiation Oncology, University of Toronto, Toronto, ON, Canada
+(5) Institute for Better Health, Trillium Health Partners, Mississauga, ON, Canada
+(6) Department of Laboratory Medicine and Pathobiology, University of Toronto, Toronto, ON, Canada
+(7) Operational Analytics Lab, Trillium Health Partners, Mississauga, ON, Canada
+(8) Department of Medical Imaging, University of Toronto, Toronto, ON, Canada
 
 This application predicts the risk of side-specific extraprostatic extension (ssEPE) using clinicopathological features
 known prior to radical prostatectomy. The proposed use is for clinicians to input de-identified patient features and the
